5b1fa7610dae498b5e3e3e5dcc8f69064e094c6e
HeresJohnny5 <ericksonjohn01@hotmail.com>
2020-07-07 00:15:50 -0400

    Create ArticlesController, Article Model and Article Show view.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_123447) do
+ActiveRecord::Schema.define(version: 2020_07_06_025304) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "title"
+    t.text "description"
   end
 
 end
